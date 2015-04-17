efd917aa4c2d72931018e8c1f8f57ea53d9eacdb
Kazune Takahashi <kazunetakahashi@gmail.com>
2015-04-17 23:18:48 +0900

    template

---
+++ gcj_template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void init() {
+}
+
+// 変数を書く
+
+void input() {
+  // cin >> ;
+}
+
+void output() {
+  cout << endl;
+}
+
+int main() {
+  init();
+  int T;
+  cin >> T;
+  for (auto t=1; t<=T; t++) {
+    cout << "Case #" << t << ": "; 
+    input();
+    output();
+  }
+}
